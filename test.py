d2404743ada6bc0bee987619a128e069c9b2f384
LemonFan-maker <2254650260@qq.com>
2023-05-28 02:56:56 +0800

    修复测试的错误

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time, random
+import time
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
 browser.get("www.baidu.com")
 time.sleep(3)
 print(browser.title)
