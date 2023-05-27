dd328ccffa0ae70719c8990f4655f3ae8b7803df
LemonFan-maker <2254650260@qq.com>
2023-05-28 03:01:32 +0800

    修复错误

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
+
+browser = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver', options=chrome_options)
+
 browser.get("www.baidu.com")
 time.sleep(3)
 print(browser.title)
