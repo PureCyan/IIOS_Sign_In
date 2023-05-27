bb198ebdb062f50ba613ac78ab54be698d8c034c
LemonFan-maker <2254650260@qq.com>
2023-05-28 02:48:56 +0800

    更改Edge为Chrome

--- login.py
+++ login.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time, random
 
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
 def login(web, pwd, acc): 
-    browser = webdriver.Edge()
+    browser = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
     browser.get(str(web))
     time.sleep(round(random.uniform(0,2),1))
     browser.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/input").send_keys(str(acc))
