4ab0bbee7bf7eee63a7fa6b8a23e354f05663b18
Cyan-离 <115965270+LemonFan-maker@users.noreply.github.com>
2023-05-29 20:19:51 +0800

    修复pwd错误

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ parser.add_argument("--pd", help="password of iios", required=True)
 parser.add_argument("--acc", help="account of iios", required=True)
 args = parser.parse_args()
 
-browser = login("https://iios.fun/#", args.pwd, args.acc)
+browser = login("https://iios.fun/#", args.pd, args.acc)
 sign("https://www.iios.fun/#/points", browser)
 questions("https://www.iios.fun/#/points", browser)
