644438ab4774a4fe5f2eb17cf9c608b72eac026f
Cyan-离 <115965270+LemonFan-maker@users.noreply.github.com>
2023-05-29 20:19:09 +0800

    修复pwd错误

--- login.py
+++ login.py
@@ -10,13 +10,13 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-def login(web, pwd, acc): 
+def login(web, pd, acc): 
     s = Service(executable_path=r"/usr/local/bin/chromedriver")
     browser = webdriver.Chrome(service=s, options=chrome_options)
     browser.get(str(web))
     time.sleep(round(random.uniform(0,2),1))
     browser.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/input").send_keys(str(acc))
-    browser.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/input").send_keys(str(pwd))
+    browser.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/input").send_keys(str(pd))
     browser.find_element(By.XPATH, "/html/body/div/div/div[3]/button").click()
     time.sleep(round(random.uniform(0,2),1))
     return browser
