d166ad1fe04b85bcbb9b78926212f13cba816382
Cyan-离 <115965270+LemonFan-maker@users.noreply.github.com>
2023-06-02 21:45:35 +0800

    更新 click_sign.py

--- click_sign.py
+++ click_sign.py
@@ -3,6 +3,6 @@ import time
 
 def sign(web, browser):
     browser.get(str(web))
-    time.sleep(3)
+    time.sleep(5)
     browser.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div[1]/div[3]").click()
-    time.sleep(2)
+    time.sleep(3)
